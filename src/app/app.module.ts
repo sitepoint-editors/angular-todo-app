5bfdb717857bc19bc23493f631f381f28e079f8f
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 16:42:18 +0200

    feat(AppRoutingModule): add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';

0b9d079ae2bd1d9cc9b9526daf1553b8d8f43ce0
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 21:58:39 +0200

    feat(json-server): update sign-in logic

--- json-server.js
+++ json-server.js
@@ -3,6 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const jwt = require('express-jwt');
+const bodyParser = require('body-parser');
 
 // Sample JWT token for demo purposes
 const jwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2l0ZVBvaW50IFJ' +
@@ -11,12 +12,20 @@ const jwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2l0ZVBvaW50I
 // Use default middlewares (CORS, static, etc)
 server.use(middlewares);
 
+// Make sure JSON bodies are parsed correctly
+server.use(bodyParser.json());
+
 // Handle sign-in requests
 server.post('/sign-in', (req, res) => {
-  res.json({
-    name: 'SitePoint Reader',
-    token: jwtToken
-  });
+  const username = req.body.username;
+  const password = req.body.password;
+  if(username === 'demo' && password === 'demo') {
+    res.json({
+      name: 'SitePoint Reader',
+      token: jwtToken
+    });
+  }
+  res.send(422, 'Invalid username and password');
 });
 
 // Protect other routes

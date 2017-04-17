3a342f5de4722e1cddcbbcd97a375617c5438b5d
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 21:24:07 +0200

    feat(AuthService): update logic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { SessionService } from './session.service';
-import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
@@ -16,19 +15,14 @@ export class AuthService {
 
   public doSignOut() {
     this.session.destroy();
-    return Observable.of(true);
   }
 
   public doSignIn(accessToken: string, name: string) {
-    if (!accessToken) {
-      return Observable.throw('Invalid access token');
-    }
-    if (!name) {
-      return Observable.throw('Invalid name');
+    if ((!accessToken) || (!name)) {
+      return;
     }
     this.session.accessToken = accessToken;
     this.session.name = name;
-    return Observable.of(true);
   }
 
 }

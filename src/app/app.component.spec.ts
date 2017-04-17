a10f4803a338370298842f0761299aeae755b72e
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 18:09:13 +0200

    refactor(AppComponent): move logic to TodosComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
-
-import { FormsModule } from '@angular/forms';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TodoDataService } from './todo-data.service';
-import { ApiService } from './api.service';
-import { ApiMockService } from './api-mock.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        FormsModule
-      ],
       declarations: [
         AppComponent
-      ],
-      providers: [
-        TodoDataService,
-        {
-          provide: ApiService,
-          useClass: ApiMockService
-        }
-      ],
-      schemas: [
-        NO_ERRORS_SCHEMA
       ]
     }).compileComponents();
   }));

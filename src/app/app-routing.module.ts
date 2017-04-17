ec7eac5e18c99c33482c3a85852eb24990b2b167
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 18:08:22 +0200

    feat(AppRoutingModule): add routes for sign-in and todos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { TodosComponent } from './todos/todos.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: 'sign-in',
+    pathMatch: 'full'
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  },
+  {
+    path: 'todos',
+    component: TodosComponent
   }
 ];
 

6c5284276e4972d2cda70fc9bfdd792fdb8f1c3c
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 18:09:32 +0200

    feat(SignInComponent): add component

---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  public frm: FormGroup;
+
+  public isBusy = false;
+  public hasFailed = false;
+  public showInputErrors = false;
+
+  constructor(
+    private api: ApiService,
+    private auth: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.frm = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  public doSignIn() {
+
+    // Make sure form values are valid
+    if (this.frm.invalid) {
+      this.showInputErrors = true;
+      return;
+    }
+
+    // Reset status
+    this.isBusy = true;
+    this.hasFailed = false;
+
+    // Grab values from form
+    const username = this.frm.get('username').value;
+    const password = this.frm.get('password').value;
+
+    // Submit request to API
+    this.api
+      .signIn(username, password)
+      .subscribe(
+        (response) => {
+          this.auth.doSignIn(
+            response.accessToken,
+            response.name
+          );
+          this.router.navigate(['todos']);
+        },
+        (error) => {
+          this.isBusy = false;
+          this.hasFailed = true;
+        }
+      );
+  }
+
+}

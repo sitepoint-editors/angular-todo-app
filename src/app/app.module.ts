43f98380e4c3470d7633fa1e12e969d2f6db2329
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 18:09:53 +0200

    feat(AuthService): add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -11,6 +11,10 @@ import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.com
 import { TodoDataService } from './todo-data.service';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { ApiService } from './api.service';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { TodosComponent } from './todos/todos.component';
+import { AuthService } from './auth.service';
+import { SessionService } from './session.service';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TodosComponent } from './todos/todos.component';
@@ -22,6 +26,9 @@ import { TodosComponent } from './todos/todos.component';
     TodoListFooterComponent,
     TodoListHeaderComponent,
     TodoListItemComponent,
+    SignInComponent,
+    TodosComponent,
+    TodoListItemComponent,
     PageNotFoundComponent,
     TodosComponent
   ],
@@ -29,9 +36,15 @@ import { TodosComponent } from './todos/todos.component';
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [TodoDataService, ApiService],
+  providers: [
+    ApiService,
+    AuthService,
+    SessionService,
+    TodoDataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

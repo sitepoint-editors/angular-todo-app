4bfd7701eb609cb66f9deed326146fee887b2ab9
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 15:00:59 +0200

    feat(json-server): add server with middleware to handle JWT

---
+++ json-server.js
@@ -0,0 +1,48 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const jwt = require('express-jwt');
+
+// Sample JWT token for demo purposes
+const jwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2l0ZVBvaW50IFJ' +
+  'lYWRlciJ9.sS4aPcmnYfm3PQlTtH14az9CGjWkjnsDyG_1ats4yYg';
+
+// Use default middlewares (CORS, static, etc)
+server.use(middlewares);
+
+// Handle sign-in requests
+server.post('sign-in', (req, res, next) => {
+  return {
+    name: 'SitePoint Reader',
+    token: jwtToken
+  };
+});
+
+// Protect other routes
+server.use((req, res, next) => {
+  if (isAuthorized(req)) {
+    console.log('Access granted');
+    next();
+  } else {
+    console.log('Access denied, invalid JWT');
+    res.sendStatus(401);
+  }
+});
+
+// API routes
+server.use(router);
+
+// Start server
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+// Check whether request is allowed
+function isAuthorized(req) {
+  let bearer = req.get('Authorization');
+  if (bearer === 'Bearer ' + jwtToken) {
+    return true;
+  }
+  return false;
+}

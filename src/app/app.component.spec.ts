cb7ef91ed83efd82f517e66b72fbd6a1404c5a9a
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 18:25:18 +0200

    feat(*): update code to fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ]

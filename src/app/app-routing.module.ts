8bc4d5c55c8583f3af49b603dcd394179c1b6093
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-04-17 18:18:24 +0200

    feat(CanActivateTodosService): add logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TodosComponent } from './todos/todos.component';
+import { CanActivateTodosService } from './can-activate-todos.service';
 import { TodosResolver } from './todos.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'todos',
+    redirectTo: 'sign-in',
     pathMatch: 'full'
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent
   },
   {
     path: 'todos',
     component: TodosComponent,
+    canActivate: [
+      CanActivateTodosService
+    ],
     resolve: {
       todos: TodosResolver
     }
@@ -27,8 +36,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [
-    TodosResolver
+    CanActivateTodosService
   ]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- src/app/can-activate-todos.service.ts
+++ src/app/can-activate-todos.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class CanActivateTodosService {
+export class CanActivateTodosService implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+  }
+
+  public canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.auth.isSignedIn()) {
+      console.error('Access denied - Redirect to sign-in page');
+      this.router.navigate(['/sign-in']);
+      return false;
+    }
+  }
 
 }

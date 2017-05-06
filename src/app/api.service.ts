3926c908ad42fa89e48c960897de9a2ec9238c72
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-05-06 11:31:02 +0200

    feat(ApiService): use environment for api URL

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { Http, Response } from '@angular/http';
 import { Todo } from './todo';
 import { Observable } from 'rxjs/Observable';
@@ -6,7 +7,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-export const API_URL = 'http://localhost:3000';
+const API_URL = environment.apiUrl;
 
 @Injectable()
 export class ApiService {

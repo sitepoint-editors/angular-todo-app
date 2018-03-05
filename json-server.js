d1f48d4cad91748c4291d701f8d3b82818ced121
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2018-03-05 14:00:12 +0100

    feat(json-server): remove obsolete dependency

--- json-server.js
+++ json-server.js
@@ -2,7 +2,6 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const jwt = require('express-jwt');
 const bodyParser = require('body-parser');
 
 // Sample JWT token for demo purposes

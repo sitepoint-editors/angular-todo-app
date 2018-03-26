6c45f24d15a6a386a980cf67dedd2565d025d322
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2018-03-26 20:29:14 +0200

    test(ApiService): update TestBed configuration

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,27 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
-import { BaseRequestOptions, Http, XHRBackend } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-import { AuthService } from 'app/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SessionService } from 'app/session.service';
 
 describe('ApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        {
-          provide: Http, useFactory: (backend, options) => {
-          return new Http(backend, options);
-        },
-          deps: [MockBackend, BaseRequestOptions]
-        },
-        MockBackend,
-        BaseRequestOptions,
-        ApiService,
-        AuthService,
-        SessionService
-      ]
+      providers: [ApiService, SessionService],
+      imports: [HttpClientTestingModule]
     });
   });
 
